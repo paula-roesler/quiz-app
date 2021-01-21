0586f53494710a0d9a22286e6ecfe5dea34c786f
Paula Rösler <paularoesler@web.de>
2021-01-21 06:18:19 +0100

    Update storybook installation

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,12 @@
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 }
+
+import '../styles/index.css'
+import './app-grid.css'
+import html from './app-grid.html'
+export default {
+  title: 'app-grid',
+  component: html,
+}
+export const base = () => html
---
+++ components/navigation.stories.js
@@ -0,0 +1,10 @@
+import '../styles/index.css'
+
+import './navigation.css'
+import navigation from './navigation.html'
+
+export default {
+  title: 'Navigation',
+}
+
+export const base = () => navigation
