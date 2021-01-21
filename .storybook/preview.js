eb97bd68682f5584d72a7b9a724393854111ed85
Paula Rösler <paularoesler@web.de>
2021-01-21 07:28:36 +0100

    Undo changers on preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,3 @@
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 }
-
-import '../styles/index.css'
-import './app-grid.css'
-import html from './app-grid.html'
-export default {
-  title: 'app-grid',
-  component: html,
-}
-export const base = () => html
