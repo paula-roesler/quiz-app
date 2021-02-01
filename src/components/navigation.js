49207a080ccddf60d1917b2bb2b92497bcf5e824
Paula Rösler <paularoesler@web.de>
2021-02-01 16:10:39 +0100

    Add ESLint

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,72 +1,23 @@
 import getByDataJs from '../js/getByDataJs'
+import getAllByDataJs from '../js/getAllByDataJs'
 
 export default function navigation() {
-  const sec_home = getByDataJs('sec_home')
-  const sec_bookmarks = getByDataJs('sec_bookmarks')
-  const sec_create = getByDataJs('sec_create')
-  const sec_profil = getByDataJs('sec_profil')
+  const pages = getAllByDataJs('page')
+  const navButtons = getAllByDataJs('nav_bt')
 
-  const bt_home = getByDataJs('bt_home')
-  const bt_bookmark = getByDataJs('bt_bookmark')
-  const bt_create = getByDataJs('bt_create')
-  const bt_profil = getByDataJs('bt_profil')
-
-  const page_title = getByDataJs('page_title')
-
-  bt_home.classList.add('active')
-  bt_home.addEventListener('click', () => {
-    sec_home.classList.remove('hidden')
-    sec_bookmarks.classList.add('hidden')
-    sec_create.classList.add('hidden')
-    sec_profil.classList.add('hidden')
-
-    bt_home.classList.add('active')
-    bt_bookmark.classList.remove('active')
-    bt_create.classList.remove('active')
-    bt_profil.classList.remove('active')
-
-    page_title.textContent = 'Home'
-  })
-
-  bt_bookmark.addEventListener('click', () => {
-    sec_home.classList.add('hidden')
-    sec_bookmarks.classList.remove('hidden')
-    sec_create.classList.add('hidden')
-    sec_profil.classList.add('hidden')
-
-    bt_home.classList.remove('active')
-    bt_bookmark.classList.add('active')
-    bt_create.classList.remove('active')
-    bt_profil.classList.remove('active')
-
-    page_title.textContent = 'Bookmarks'
-  })
-
-  bt_create.addEventListener('click', () => {
-    sec_home.classList.add('hidden')
-    sec_bookmarks.classList.add('hidden')
-    sec_create.classList.remove('hidden')
-    sec_profil.classList.add('hidden')
-
-    bt_home.classList.remove('active')
-    bt_bookmark.classList.remove('active')
-    bt_create.classList.add('active')
-    bt_profil.classList.remove('active')
-
-    page_title.textContent = 'Create'
-  })
-
-  bt_profil.addEventListener('click', () => {
-    sec_home.classList.add('hidden')
-    sec_bookmarks.classList.add('hidden')
-    sec_create.classList.add('hidden')
-    sec_profil.classList.remove('hidden')
-
-    bt_home.classList.remove('active')
-    bt_bookmark.classList.remove('active')
-    bt_create.classList.remove('active')
-    bt_profil.classList.add('active')
-
-    page_title.textContent = 'Profile'
+  navButtons.forEach(button => {
+    const clickedButtonName = button.dataset.name
+    button.addEventListener('click', () => {
+      pages.forEach(page => {
+        const pageName = page.dataset.name
+        page.classList.toggle('hidden', clickedButtonName !== pageName)
+      })
+      navButtons.forEach(button => {
+        button.classList.toggle(
+          'active',
+          clickedButtonName === button.dataset.name
+        )
+      })
+    })
   })
 }
