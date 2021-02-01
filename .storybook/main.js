d4b9ac79855efd5810cfbfb85efa444bf5b0d1c7
Paula Rösler <paularoesler@web.de>
2021-02-01 11:41:39 +0100

    form-counter not complete

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,3 +2,8 @@ module.exports = {
   stories: ['../**/*.stories.js'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 }
+/*
+module.exports = {
+  stories: ['../src/components/**/*.stories.js'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+}
